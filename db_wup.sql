75d2e92d73b7db868d9078f3265ef3319dae15d2
JandelMars <39218158+Novy13@users.noreply.github.com>
2022-01-05 11:33:41 +0800

    Update db_wup.sql

--- db_wup.sql
+++ db_wup.sql
@@ -18,7 +18,7 @@
 
 -- Dumping data for table db_wup.messagelog: ~42 rows (approximately)
 /*!40000 ALTER TABLE `messagelog` DISABLE KEYS */;
-INSERT IGNORE INTO `messagelog` (`Id`, `SendTime`, `ReceiveTime`, `StatusCode`, `StatusText`, `MessageTo`, `MessageFrom`, `MessageText`, `MessageType`, `MessageId`, `ErrorCode`, `ErrorText`, `Gateway`, `MessageParts`, `MessagePDU`, `Connector`, `UserId`, `UserInfo`) VALUES
+INSERT INTO `messagelog` (`Id`, `SendTime`, `ReceiveTime`, `StatusCode`, `StatusText`, `MessageTo`, `MessageFrom`, `MessageText`, `MessageType`, `MessageId`, `ErrorCode`, `ErrorText`, `Gateway`, `MessageParts`, `MessagePDU`, `Connector`, `UserId`, `UserInfo`) VALUES
 	(26, '2021-10-08 14:12:47', NULL, 200, NULL, '+639382715180', '1577A4EB-2491-4C73-885F-E2E7BE770B7A', 'You have an Announcement notification from WUP-Maria.  <br> Title:  Examination Preliminary <br> What:asdasdasdsadasdsadasdsadasdasdasdsadasd <br> When: 2021-10-27T14:12<br> Where: WUP Campus', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 	(27, '2021-10-08 14:12:48', NULL, 200, NULL, '+639090909090', 'E36A4034-FBD6-43A5-8743-91E777907C6A', 'You have an Announcement notification from WUP-Maria.  <br> Title:  Examination Preliminary <br> What:asdasdasdsadasdsadasdsadasdasdasdsadasd <br> When: 2021-10-27T14:12<br> Where: WUP Campus', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 	(28, '2021-10-08 14:14:14', NULL, 200, NULL, '+639185292671', 'DAF6746C-4C71-4060-84C7-76D185E2C0C0', 'You have an Event notification from WUP-Maria. <br>Title: Valentine Day Celebration <br>What: We are helding valentine Celebration come and join us <br>When: 2021-10-26T14:14 <br>When: WUP Covered Court', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
@@ -63,39 +63,53 @@ INSERT IGNORE INTO `messagelog` (`Id`, `SendTime`, `ReceiveTime`, `StatusCode`, 
 	(67, '2021-11-22 20:54:53', NULL, 300, NULL, '+639182323232', NULL, 'You have an Event notification from WUP-Maria Aurora. <br>Title: New Event 23 <br>What: GHAPSDPA <br>When: 2022-01-06T20:54 <br>Where: Garte', NULL, '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 /*!40000 ALTER TABLE `messagelog` ENABLE KEYS */;
 
--- Dumping data for table db_wup.messageout: ~0 rows (approximately)
+-- Dumping data for table db_wup.messageout: ~9 rows (approximately)
 /*!40000 ALTER TABLE `messageout` DISABLE KEYS */;
+INSERT INTO `messageout` (`Id`, `MessageTo`, `MessageFrom`, `MessageText`, `MessageType`, `Gateway`, `UserId`, `UserInfo`, `Priority`, `Scheduled`, `ValidityPeriod`, `IsSent`, `IsRead`) VALUES
+	(1, '+639090909090', 'Admin', 'You have an Announcement notification from WUP-Maria Aurora.  <br> Title:  General Meeting for Parents and Friends <br> What:Wala ahahahsdhahsd <br> When: 2021-12-09T00:18<br> Where: WUP AURORA Campus', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0),
+	(2, '+63', 'Admin', 'You have an Announcement notification from WUP-Maria Aurora.  <br> Title:  General Meeting for Parents and Friends <br> What:Wala ahahahsdhahsd <br> When: 2021-12-09T00:18<br> Where: WUP AURORA Campus', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0),
+	(3, '+639182323232', 'Admin', 'You have an Announcement notification from WUP-Maria Aurora.  <br> Title:  General Meeting for Parents and Friends <br> What:Wala ahahahsdhahsd <br> When: 2021-12-09T00:18<br> Where: WUP AURORA Campus', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0),
+	(4, '+639090909090', 'Admin', 'You have an Event notification from WUP-Maria Aurora. <br>Title: New Event 13 <br>What: Walang pasok <br>When: 2021-12-29T22:17 <br>Where: Aurora Wup', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0),
+	(5, '+63', 'Admin', 'You have an Event notification from WUP-Maria Aurora. <br>Title: New Event 13 <br>What: Walang pasok <br>When: 2021-12-29T22:17 <br>Where: Aurora Wup', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0),
+	(6, '+639182323232', 'Admin', 'You have an Event notification from WUP-Maria Aurora. <br>Title: New Event 13 <br>What: Walang pasok <br>When: 2021-12-29T22:17 <br>Where: Aurora Wup', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0),
+	(7, '+639182323232', 'Admin', 'You have an Event notification from WUP-Maria Aurora. <br>Title: New Event 13 <br>What: Walang pasok <br>When: 2021-12-29T22:17 <br>Where: Aurora Wup', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0),
+	(8, '+63', 'Admin', 'You have an Event notification from WUP-Maria Aurora. <br>Title: New Event 2555 <br>What: wawawawawa <br>When: 2022-01-12T20:48 <br>Where: Garte Aw', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0),
+	(9, '+639182323232', 'Admin', 'You have an Event notification from WUP-Maria Aurora. <br>Title: New Event 2555 <br>What: wawawawawa <br>When: 2022-01-12T20:48 <br>Where: Garte Aw', NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0);
 /*!40000 ALTER TABLE `messageout` ENABLE KEYS */;
 
--- Dumping data for table db_wup.tblannouncement: ~2 rows (approximately)
+-- Dumping data for table db_wup.tblannouncement: ~4 rows (approximately)
 /*!40000 ALTER TABLE `tblannouncement` DISABLE KEYS */;
-INSERT IGNORE INTO `tblannouncement` (`ANNOUNCEMENTID`, `ANNOUNCEMENT_TEXT`, `ANNOUNCEMENT_WHAT`, `ANNOUNCEMENT_WHEN`, `ANNOUNCEMENT_WHERE`, `DATEPOSTED`) VALUES
+INSERT INTO `tblannouncement` (`ANNOUNCEMENTID`, `ANNOUNCEMENT_TEXT`, `ANNOUNCEMENT_WHAT`, `ANNOUNCEMENT_WHEN`, `ANNOUNCEMENT_WHERE`, `DATEPOSTED`) VALUES
 	(202100058, 'General Announcement', 'sdaasdasd', '2021-11-19 20:51:00', 'WUP AURORA', '2021-11-22 12:51:52'),
 	(202100059, 'General Meeting for Parents', 'SDdsDASDASD', '2021-11-26 20:52:00', 'WUP AURORA', '2021-11-22 12:52:11'),
-	(202100060, 'General Announcement 33', 'ASDASDAS', '2022-01-05 20:52:00', 'WUP AURORA Campus', '2021-11-22 12:52:33');
+	(202100060, 'General Announcement 33', 'ASDASDAS', '2022-01-05 20:52:00', 'WUP AURORA Campus', '2021-11-22 12:52:33'),
+	(202100062, 'General Meeting for Parents and Friends', 'Wala ahahahsdhahsd', '2021-12-09 00:18:00', 'WUP AURORA Campus', '2021-12-11 16:18:19');
 /*!40000 ALTER TABLE `tblannouncement` ENABLE KEYS */;
 
 -- Dumping data for table db_wup.tblautonumbers: ~2 rows (approximately)
 /*!40000 ALTER TABLE `tblautonumbers` DISABLE KEYS */;
-INSERT IGNORE INTO `tblautonumbers` (`AUTOID`, `AUTOSTART`, `AUTOEND`, `AUTOINC`, `AUTOKEY`) VALUES
+INSERT INTO `tblautonumbers` (`AUTOID`, `AUTOSTART`, `AUTOEND`, `AUTOINC`, `AUTOKEY`) VALUES
 	(10, '000', '27', 1, 'bookingid'),
-	(12, '000', '62', 1, 'BLOGID');
+	(12, '000', '65', 1, 'BLOGID');
 /*!40000 ALTER TABLE `tblautonumbers` ENABLE KEYS */;
 
--- Dumping data for table db_wup.tblblogpost: ~6 rows (approximately)
+-- Dumping data for table db_wup.tblblogpost: ~9 rows (approximately)
 /*!40000 ALTER TABLE `tblblogpost` DISABLE KEYS */;
-INSERT IGNORE INTO `tblblogpost` (`BLOGID`, `BLOGS`, `BLOG_WHAT`, `BLOG_WHEN`, `BLOG_WHERE`, `DATEPOSTED`, `CATEGORY`, `AUTHOR`) VALUES
+INSERT INTO `tblblogpost` (`BLOGID`, `BLOGS`, `BLOG_WHAT`, `BLOG_WHEN`, `BLOG_WHERE`, `DATEPOSTED`, `CATEGORY`, `AUTHOR`) VALUES
 	(202100056, 'New Event', 'fasdfsdfsdf', '2021-11-17T20:50', 'Aurora Wup', '2021-11-22 20:50:46', 'EVENT', 'Awit'),
 	(202100057, 'New Event 2', 'asdasdasdasd', '2021-12-03T20:51', 'Aurora Wup', '2021-11-22 20:51:27', 'EVENT', 'Awit'),
 	(202100058, 'General Announcement', 'sdaasdasd', '2021-11-19T20:51', 'WUP AURORA', '2021-11-22 20:51:52', 'ANNOUNCEMENT', 'Awit'),
 	(202100059, 'General Meeting for Parents', 'SDdsDASDASD', '2021-11-26T20:52', 'WUP AURORA', '2021-11-22 20:52:11', 'ANNOUNCEMENT', 'Awit'),
 	(202100060, 'General Announcement 33', 'ASDASDAS', '2022-01-05T20:52', 'WUP AURORA Campus', '2021-11-22 20:52:33', 'ANNOUNCEMENT', 'Awit'),
-	(202100061, 'New Event 23', 'GHAPSDPA', '2022-01-06T20:54', 'Garte', '2021-11-22 20:54:34', 'EVENT', 'Awit');
+	(202100061, 'New Event 23', 'GHAPSDPA', '2022-01-06T20:54', 'Garte', '2021-11-22 20:54:34', 'EVENT', 'Awit'),
+	(202100062, 'General Meeting for Parents and Friends', 'Wala ahahahsdhahsd', '2021-12-09T00:18', 'WUP AURORA Campus', '2021-12-12 00:18:19', 'ANNOUNCEMENT', 'Awit'),
+	(202100063, 'New Event 13', 'Walang pasok', '2021-12-29T22:17', 'Aurora Wup', '2021-12-13 22:17:24', 'EVENT', 'Awit'),
+	(202200064, 'New Event 2555', 'wawawawawa', '2022-01-12T20:48', 'Garte Aw', '2022-01-03 20:49:02', 'EVENT', 'Awit1');
 /*!40000 ALTER TABLE `tblblogpost` ENABLE KEYS */;
 
 -- Dumping data for table db_wup.tblcourse: ~5 rows (approximately)
 /*!40000 ALTER TABLE `tblcourse` DISABLE KEYS */;
-INSERT IGNORE INTO `tblcourse` (`COURSEID`, `COURSE`, `DESCRIPTION`) VALUES
+INSERT INTO `tblcourse` (`COURSEID`, `COURSE`, `DESCRIPTION`) VALUES
 	(3, 'BSED', 'Bachelor of Science in Secondary Education'),
 	(4, 'BSCS', 'College of Computer Science'),
 	(5, 'BSCRIM', 'College of Crimal Justice Education'),
@@ -105,29 +119,29 @@ INSERT IGNORE INTO `tblcourse` (`COURSEID`, `COURSE`, `DESCRIPTION`) VALUES
 
 -- Dumping data for table db_wup.tbldepartment: ~3 rows (approximately)
 /*!40000 ALTER TABLE `tbldepartment` DISABLE KEYS */;
-INSERT IGNORE INTO `tbldepartment` (`DEPARTMENTID`, `DEPARTMENT`, `DESCRIPTION`) VALUES
+INSERT INTO `tbldepartment` (`DEPARTMENTID`, `DEPARTMENT`, `DESCRIPTION`) VALUES
 	(3, 'COLLEGE', 'Wesleyan College Department'),
 	(4, 'SHS ', 'SENIOR HIGH SCHOOL DEPARTMENT'),
 	(5, 'JHS', 'JUNIOR HIGH SCHOOL DEPARTMENT');
 /*!40000 ALTER TABLE `tbldepartment` ENABLE KEYS */;
 
--- Dumping data for table db_wup.tblevent: ~2 rows (approximately)
+-- Dumping data for table db_wup.tblevent: ~5 rows (approximately)
 /*!40000 ALTER TABLE `tblevent` DISABLE KEYS */;
-INSERT IGNORE INTO `tblevent` (`EVENTID`, `EVENT_TEXT`, `EVENT_WHAT`, `EVENT_WHEN`, `EVENT_WHERE`, `DATEPOSTED`) VALUES
+INSERT INTO `tblevent` (`EVENTID`, `EVENT_TEXT`, `EVENT_WHAT`, `EVENT_WHEN`, `EVENT_WHERE`, `DATEPOSTED`) VALUES
 	(202100056, 'New Event', 'fasdfsdfsdf', '2021-11-17 20:50:00', 'Aurora Wup', '2021-11-22 12:50:46'),
 	(202100057, 'New Event 2', 'asdasdasdasd', '2021-12-03 20:51:00', 'Aurora Wup', '2021-11-22 12:51:27'),
-	(202100061, 'New Event 23', 'GHAPSDPA', '2022-01-06 20:54:00', 'Garte', '2021-11-22 12:54:34');
+	(202100061, 'New Event 23', 'GHAPSDPA', '2022-01-06 20:54:00', 'Garte', '2021-11-22 12:54:34'),
+	(202100063, 'New Event 13', 'Walang pasok', '2021-12-29 22:17:00', 'Aurora Wup', '2021-12-13 14:17:24'),
+	(202200064, 'New Event 2555', 'wawawawawa', '2022-01-12 20:48:00', 'Garte Aw', '2022-01-03 12:49:02');
 /*!40000 ALTER TABLE `tblevent` ENABLE KEYS */;
 
 -- Dumping data for table db_wup.tblfaculty: ~0 rows (approximately)
 /*!40000 ALTER TABLE `tblfaculty` DISABLE KEYS */;
-INSERT IGNORE INTO `tblfaculty` (`INCID`, `IDNO`, `FNAME`, `MNAME`, `LNAME`, `DEPARTMENT`, `PHONE`, `PROIMAGE`, `FORGOTPASSWORD`) VALUES
-	(9, '323232323', 'Augustus', 'Ceasar', 'Thirdy', '', '09182323232', NULL, NULL);
 /*!40000 ALTER TABLE `tblfaculty` ENABLE KEYS */;
 
 -- Dumping data for table db_wup.tblstrand: ~4 rows (approximately)
 /*!40000 ALTER TABLE `tblstrand` DISABLE KEYS */;
-INSERT IGNORE INTO `tblstrand` (`STRANDID`, `STRAND`, `DESCRIPTION1`) VALUES
+INSERT INTO `tblstrand` (`STRANDID`, `STRAND`, `DESCRIPTION1`) VALUES
 	(1, 'STEM', 'STEM'),
 	(2, 'ABM', 'ABM'),
 	(3, 'HUMSS', 'HUMSS'),
@@ -136,14 +150,14 @@ INSERT IGNORE INTO `tblstrand` (`STRANDID`, `STRAND`, `DESCRIPTION1`) VALUES
 
 -- Dumping data for table db_wup.tblstudent: ~2 rows (approximately)
 /*!40000 ALTER TABLE `tblstudent` DISABLE KEYS */;
-INSERT IGNORE INTO `tblstudent` (`INCID`, `IDNO`, `FNAME`, `MNAME`, `LNAME`, `PHONE`, `DEPARTMENT`, `YLVL`, `SEC_BLOCK`, `COURSE`, `USERNAME`, `STUDPASS`, `PROIMAGE`, `FORGOTPASSAUTH`) VALUES
-	(21, '612361263', 'August', 'Mana', 'Lona', '09090909090', '3', '4th Year', 'Block 3', '3', 'D3$LbrjPf7Tz', 'Fb!$8n', 'student_image/239997704_820843678622654_955177988405425248_n.jpg', NULL),
-	(22, '6', 'Super Admin', '', '', '', '', '', '', '', 'SU', '3fda92826b531fc6ebf7e1f511e9605dd159e0e2', NULL, NULL);
+INSERT INTO `tblstudent` (`INCID`, `IDNO`, `FNAME`, `MNAME`, `LNAME`, `PHONE`, `DEPARTMENT`, `YLVL`, `SEC_BLOCK`, `COURSE`, `USERNAME`, `STUDPASS`, `PROIMAGE`, `FORGOTPASSAUTH`) VALUES
+	(22, '6', 'Super Admin', '', '', '', '', '', '', '', 'SU', '3fda92826b531fc6ebf7e1f511e9605dd159e0e2', NULL, NULL),
+	(23, '180422332', 'Minaqu', 'Wara', 'Lastimosa', '09182323232', '3', '3rd Year', 'Block 3', '4', '2J#zE*fDiQSx', 'iCpc#T', NULL, NULL);
 /*!40000 ALTER TABLE `tblstudent` ENABLE KEYS */;
 
--- Dumping data for table db_wup.tblstudentnotif: ~13 rows (approximately)
+-- Dumping data for table db_wup.tblstudentnotif: ~22 rows (approximately)
 /*!40000 ALTER TABLE `tblstudentnotif` DISABLE KEYS */;
-INSERT IGNORE INTO `tblstudentnotif` (`NotificationID`, `IDNO`, `BLOGID`, `TYPE`) VALUES
+INSERT INTO `tblstudentnotif` (`NotificationID`, `IDNO`, `BLOGID`, `TYPE`) VALUES
 	(26, '123123123', '', 'Event'),
 	(27, '123123123', '', 'Announcement'),
 	(28, '612361263', '', 'Announcement'),
@@ -158,13 +172,20 @@ INSERT IGNORE INTO `tblstudentnotif` (`NotificationID`, `IDNO`, `BLOGID`, `TYPE`
 	(37, '612361263', '202100060', 'Announcement'),
 	(38, '6', '202100060', 'Announcement'),
 	(39, '612361263', '202100061', 'Event'),
-	(40, '6', '202100061', 'Event');
+	(40, '6', '202100061', 'Event'),
+	(41, '612361263', '202100062', 'Announcement'),
+	(42, '6', '202100062', 'Announcement'),
+	(43, '612361263', '202100063', 'Event'),
+	(44, '6', '202100063', 'Event'),
+	(45, '180422332', '202100063', 'Event'),
+	(46, '6', '202200064', 'Event'),
+	(47, '180422332', '202200064', 'Event');
 /*!40000 ALTER TABLE `tblstudentnotif` ENABLE KEYS */;
 
 -- Dumping data for table db_wup.tblusers: ~2 rows (approximately)
 /*!40000 ALTER TABLE `tblusers` DISABLE KEYS */;
-INSERT IGNORE INTO `tblusers` (`USERID`, `NAME`, `UEMAIL`, `PASS`, `TYPE`) VALUES
-	(4, 'Awit', 'admin', '38f078a81a2b033d197497af5b77f95b50bfcfb8', 'Administrator'),
+INSERT INTO `tblusers` (`USERID`, `NAME`, `UEMAIL`, `PASS`, `TYPE`) VALUES
+	(4, 'Awit1', 'admin', '94bce5f1f43a5444ad88f9ae49db68e024d53d70', 'Administrator'),
 	(5, 'Super Admin', 'SU', '3fda92826b531fc6ebf7e1f511e9605dd159e0e2', 'Administrator');
 /*!40000 ALTER TABLE `tblusers` ENABLE KEYS */;
 
